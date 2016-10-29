f275c4b4a18aaa361f7de80724986c4667b7bea0
mario <github@lxxxvi.ch>
2016-10-30 00:03:01 +0100

    Configuration and tests

--- lib/choker.rb
+++ lib/choker.rb
@@ -1,4 +1,23 @@
 require 'choker/version'
+require 'choker/configuration'
 
 module Choker
+  class << self
+    def configure(options = {})
+      options.each do |k, v|
+        next unless configuration.respond_to?("#{k}=")
+        configuration.send("#{k}=", v)
+      end
+
+      yield configuration if block_given?
+    end
+
+    def configuration
+      @config ||= Choker::Configuration.new
+    end
+
+    def reset_to_defaults
+      @config = Choker::Configuration.new
+    end
+  end
 end
